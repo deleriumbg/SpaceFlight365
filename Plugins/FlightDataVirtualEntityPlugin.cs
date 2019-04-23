8846f8be1746f91007109691fed98670c698273a
deleriumbg <toolmail@abv.bg>
2019-04-23 20:58:13 +0300

    Add FlightDataVirtualEntitzPlugin

---
+++ Plugins/FlightDataVirtualEntityPlugin.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SF365.Plugins
+{
+    public class FlightDataVirtualEntityPlugin : BasePlugin
+    {
+        public FlightDataVirtualEntityPlugin() : base(typeof(FlightDataVirtualEntityPlugin))
+        {
+
+        }
+
+        protected override void ExecuteCrmPlugin(LocalPluginContext localcontext)
+        {
+            switch (localcontext.PluginExecutionContext.MessageName)
+            {
+                case "RetrieveMultiple":
+                    RetrieveMultiple(localcontext);
+                    break;
+                case "Retrieve":
+                    Retrieve(localcontext);
+                    break;
+            }
+        }
+
+        private void Retrieve(LocalPluginContext localcontext)
+        {
+            var target = (EntityReference)localcontext.PluginExecutionContext.InputParameters["Target"];
+            var record = GetFlightData(target.Id, null).FirstOrDefault();
+            localcontext.PluginExecutionContext.OutputParameters["BusinessEntity"] = record;
+        }
+
+        private void RetrieveMultiple(LocalPluginContext localcontext)
+        {
+            var query = (QueryBase)localcontext.PluginExecutionContext.InputParameters["Query"];
+            if (query == null)
+                return;
+
+            // Get the flight number query
+            var expression = query as QueryExpression;
+            var filter = expression.Criteria.Conditions.FirstOrDefault(c => c.AttributeName == "sf365_name");
+            var flightnumber = filter?.Values[0].ToString();
+
+            var data = GetFlightData(null, flightnumber);
+
+            localcontext.PluginExecutionContext.OutputParameters["BusinessEntityCollection"] =
+                new EntityCollection(data);
+        }
+
+        private List<Entity> GetFlightData(Guid? flightid, string flightnumber)
+        {
+            var random = new Random();
+
+            var data = new List<Entity>
+                {
+                    new Entity("sf365_flightdata")
+                    {
+                        ["sf365_flightdataid"] = new Guid("00000000-0000-0000-0000-000000000111"),
+                        ["sf365_name"] = "111",
+                        ["sf365_latitude"] = (decimal)random.NextDouble(),
+                        ["sf365_longitude"] = (decimal)random.NextDouble()
+                    },
+                    new Entity("sf365_flightdata")
+                    {
+                        ["sf365_flightdataid"] = new Guid("00000000-0000-0000-0000-000000000112"),
+                        ["sf365_name"] = "112",
+                        ["sf365_latitude"] = (decimal)random.NextDouble(),
+                        ["sf365_longitude"] = (decimal)random.NextDouble()
+                    }
+            };
+
+            if (flightid != null)
+            {
+                return data
+                    .Where(d => d.GetAttributeValue<Guid>("sf365_flightdataid") == flightid)
+                    .ToList();
+            }
+            if (!string.IsNullOrEmpty(flightnumber))
+            {
+                return data
+                    .Where(d => d.GetAttributeValue<string>("sf365_name")
+                        .EndsWith(flightnumber))
+                    .ToList();
+            }
+
+            return data;
+        }
+    }
+}
